f1750450644d606682de37f289237b83848a2912
Salah <salah@Salahs-Laptop.local>
2024-09-12 18:09:30 +0300

    initail

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 ///////////////////////
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://world-q468.vercel.app"],
+    origin: ["https://world-q468.vercel.app"],
     credentials: true,
     methods: "*",
   })
