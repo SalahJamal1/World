c7f13cf4ecda3193ae7ed16daa1bd4b8b76fd054
Salah <salah@Salahs-Laptop.local>
2024-09-12 18:34:55 +0300

    initail

--- client/src/features/app/User.jsx
+++ client/src/features/app/User.jsx
@@ -1,22 +1,29 @@
 import { useSelector } from "react-redux";
 import { Getusers } from "../../hooks/Getuser";
 import { HandelLogouts } from "../../hooks/Signout";
+import Loading from "../../ui/Loading";
 
 function User() {
-  const { user } = useSelector((store) => store.userSlice);
+  const { user, loader } = useSelector((store) => store.userSlice);
   Getusers();
   const { handelLogout } = HandelLogouts();
 
   return (
     <div className="flex space-x-2 bg-slate-600 rounded-lg px-2 py-2 absolute top-2 right-2 z-[999] items-center shadow-lg">
-      <img src={user.avatar} alt="user" className="h-12 rounded-full" />
-      <h3 className="text-slate-100">Welcome,{user.name}</h3>
-      <button
-        className="px-2 py-1 rounded-md border border-slate-100 text-slate-100 uppercase text-sm"
-        onClick={handelLogout}
-      >
-        Logout
-      </button>
+      {loader ? (
+        <Loading />
+      ) : (
+        <>
+          <img src={user.avatar} alt="user" className="h-12 rounded-full" />
+          <h3 className="text-slate-100">Welcome,{user.name}</h3>
+          <button
+            className="px-2 py-1 rounded-md border border-slate-100 text-slate-100 uppercase text-sm"
+            onClick={handelLogout}
+          >
+            Logout
+          </button>
+        </>
+      )}
     </div>
   );
 }
