264c33412afcb038f9ba722b10cc14ce8c210cc1
SalahJamal1 <salahabufarha1996@outlook.com>
2025-05-23 18:09:51 +0300

    deployment

--- client/src/components/City.jsx
+++ client/src/components/City.jsx
@@ -17,15 +17,15 @@ function City({ city }) {
       to={`${city?._id}?lat=${city?.position?.lat}&lng=${city?.position?.lng}`}
     >
       <li className="flex items-center border border-green-500 px-2 py-1 rounded-2xl border-l-green-500 border-l-8 justify-between flex-wrap space-x-2 space-y-1">
-        <span className="text-3xl text-slate-100">{city?.emoji}</span>
-        <span className="text-sm text-slate-100 font-semibold">
+        <span className="lg:text-3xl text-slate-100">{city?.emoji}</span>
+        <span className="lg:text-sm text-slate-100 font-semibold">
           {city?.cityName}
         </span>
-        <span className="text-sm text-slate-100">{time}</span>
+        <span className="lg:text-sm text-slate-100">{time}</span>
         <button
           disabled={x}
           onClick={handelDelete}
-          className="border-none bg-slate-500 px-2 py-1 rounded-full text-[12px]"
+          className="border-none bg-slate-500 px-2 py-1 rounded-full lg:text-[12px]"
         >
           ❌
         </button>
